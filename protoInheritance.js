bf8d6af68055f5a30d15c5eb912b9bf213f24bf6
João Victor Ramires Guimarães Brito <joaoramires.engsoft@gmail.com>
2019-12-12 16:06:11 -0300

    Implement Polymorphism - this is really tricky :)

--- protoInheritance.js
+++ protoInheritance.js
@@ -7,14 +7,24 @@ function HtmlElement() {
   this.click = () => console.log('Element Clicked.');
 }
 
+// This is a method defined in HtmlElement PROTOTYPE (its parent object.)
 HtmlElement.prototype.focus = () => console.log('Element Focued.');
 
 function HtmlSelectElement(items = []) {
   this.items = items;
   this.addItem = item => this.items.push(item);
   this.removeItem = item => this.items.splice(this.items.indexOf(item), 1);
+  this.render = () => {
+    return `
+    <select>${this.items.map(item => `
+      <option>${item}</option>`).join('')}
+    </select>`;
+  }
 }
 
+
+// Manually set once we want the click method which is defined in HtmlElement INSTANCE.
+// Otherwise we would gain access only to focus method.
 HtmlSelectElement.prototype = new HtmlElement();
 HtmlSelectElement.prototype.constructor = HtmlSelectElement;
 
@@ -22,4 +32,23 @@ HtmlSelectElement.prototype.click = () => console.log('Select Clicked.');
 HtmlSelectElement.prototype.focus = () => console.log('Select Focued.');
 
 const e = new HtmlElement();
-const s = new HtmlSelectElement();
+const s = new HtmlSelectElement([1,2,3]);
+
+function HtmlImageElement(src) {
+  this.src = src;
+  this.render = () => `<img src="${this.src}" />`
+}
+
+HtmlImageElement.prototype = new HtmlElement();
+HtmlImageElement.prototype.constructor = HtmlImageElement;
+
+const i = new HtmlImageElement();
+
+const elements = [
+  new HtmlSelectElement([4,5,6,]),
+  new HtmlImageElement('http://')
+]
+
+// Look how render behaves diff depending on the element nature.
+for (let element of elements)
+  console.log(element.render());
